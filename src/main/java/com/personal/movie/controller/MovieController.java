177da1c4ccb73f2eb8a7c8fbb502a4ad7cef22b6
wndgndi <wndgn456@gmail.com>
2024-01-02 22:05:11 +0900

    feat: 인물로 영화 검색, 키워드로 제목 검색, 영화 CRUD 구현
    - getMovieByKeyword: 원제목과 제목에 해당 키워드가 포함되어 있는 영화가 존재하면 db에서 가져오고, 없으면 API를 통해 데이터를 저장, 리스트에 추가
    
    - getMovieByPerson: TMDB API를 통해 인물에 따른 영화 데이터를 가져오고, db에 존재하지 않는 영화면 저장, 리스트에 추가하고 db에 존재하는 영화면 저장하지 않고 리스트에 추가

--- src/main/java/com/personal/movie/controller/MovieController.java
+++ src/main/java/com/personal/movie/controller/MovieController.java
@@ -1,10 +1,20 @@
 package com.personal.movie.controller;
 
+import com.personal.movie.dto.MovieDto;
 import com.personal.movie.service.MovieService;
+import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,8 +27,43 @@ public class MovieController {
     @Value("${movie.secret}")
     private String apiKey;
 
-    @PostMapping("/popular")
-    public void getPopularMovies() {
-        movieService.getPopularMovie(apiKey);
+    @PostMapping
+    public ResponseEntity<MovieDto> insertMovie(@RequestBody MovieDto movieDto) {
+        return ResponseEntity.ok(movieService.insertMovie(movieDto));
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDto> getMovie(@PathVariable Long id) {
+        return ResponseEntity.ok(movieService.getMovie(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieDto> updateMovie(@PathVariable Long id,
+        @RequestBody MovieDto movieDto) {
+        return ResponseEntity.ok(movieService.updateMovie(id, movieDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<MovieDto> deleteMovie(@PathVariable Long id) {
+        return ResponseEntity.ok(movieService.deleteMovie(id));
+    }
+
+    @PostMapping("/popular")
+    public ResponseEntity<List<MovieDto>> insertPopularMovies() {
+        return ResponseEntity.ok(movieService.getPopularMovies(apiKey));
+    }
+
+    @PostMapping("/keyword")
+    public ResponseEntity<List<MovieDto>> getMovieByKeyword(@RequestParam String keyword)
+        throws IOException {
+        return ResponseEntity.ok(movieService.getMovieByKeyword(apiKey, keyword));
+    }
+
+    @PostMapping("/person")
+    public ResponseEntity<List<MovieDto>> getMovieByPerson(@RequestParam String personName)
+        throws IOException {
+
+        return ResponseEntity.ok(movieService.getMovieByPerson(apiKey, personName));
+    }
+
 }
--- src/main/java/com/personal/movie/service/MovieService.java
+++ src/main/java/com/personal/movie/service/MovieService.java
@@ -2,16 +2,23 @@ package com.personal.movie.service;
 
 import com.google.gson.Gson;
 import com.personal.movie.domain.Movie;
+import com.personal.movie.domain.constants.ErrorCode;
 import com.personal.movie.dto.MovieDto;
 import com.personal.movie.dto.response.MovieApiResponse;
+import com.personal.movie.dto.response.MovieByPersonResponse;
 import com.personal.movie.dto.response.MovieResultResponse;
+import com.personal.movie.dto.response.PersonResponse;
+import com.personal.movie.exception.CustomException;
 import com.personal.movie.repository.MovieRepository;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -26,8 +33,39 @@ public class MovieService {
     private final MovieRepository movieRepository;
     private final Gson gson;
 
-    public void getPopularMovie(String apiKey) {
-        int page = 1;
+    public MovieDto insertMovie(MovieDto movieDto) {
+        if (movieRepository.existsByMovieId(movieDto.getMovieId())) {
+            throw new CustomException(ErrorCode.ALREADY_EXIST_MOVIE);
+        }
+        movieRepository.save(movieDto.toEntity());
+        return movieDto;
+    }
+
+    public MovieDto getMovie(Long id) {
+        return MovieDto.fromEntity(movieRepository.findById(id)
+            .orElseThrow(() -> new CustomException(ErrorCode.MOVIE_NOT_FOUND)));
+    }
+
+    public MovieDto updateMovie(Long id, MovieDto movieDto) {
+        Movie movie = movieRepository.findById(id)
+            .orElseThrow(() -> new CustomException(ErrorCode.MOVIE_NOT_FOUND));
+
+        movie.updateMovie(movieDto);
+
+        return MovieDto.fromEntity(movieRepository.save(movie));
+    }
+
+    public MovieDto deleteMovie(Long id) {
+        Movie movie = movieRepository.findById(id)
+            .orElseThrow(() -> new CustomException(ErrorCode.MOVIE_NOT_FOUND));
+        movieRepository.deleteById(id);
+
+        return MovieDto.fromEntity(movie);
+    }
+
+    public List<MovieDto> getPopularMovies(String apiKey) {
+        int page = 50;
+        List<MovieDto> storeMovies = new ArrayList<>();
 
         try {
             HttpURLConnection connection = null;
@@ -50,8 +88,8 @@ public class MovieService {
                 }
 
                 MovieApiResponse MovieApiResponse = new Gson().fromJson(response.toString(),
-                    MovieApiResponse.class);
-                saveMovies(MovieApiResponse.getResults());
+                    com.personal.movie.dto.response.MovieApiResponse.class);
+                storeMovies.addAll(saveMovies(MovieApiResponse.getResults()));
             }
             br.close();
             connection.disconnect();
@@ -59,14 +97,158 @@ public class MovieService {
             e.printStackTrace();
         }
         log.warn("저장 완료");
+        return storeMovies;
     }
-    
-    private void saveMovies(List<MovieResultResponse> movieResultList) {
+
+    public List<MovieDto> getMovieByKeyword(String apiKey, String keyword) throws IOException {
+        // 원제목과 제목에 키워드가 포함된 영화들을 db 에서 조회
+        List<Movie> movies = movieRepository.findByTitleContainingIgnoreCaseOrOriginalTitleContainingIgnoreCase(
+            keyword, keyword).orElse(Collections.emptyList());
+
+        // 영화들이 db 에 존재한다면 가져오고, 존재하지 않는다면 TMDB API 를 사용해서 영화를 저장하고 반환
+        if (!movies.isEmpty()) {
+            return movies.stream().map(MovieDto::fromEntity).toList();
+        } else {
+            int currentPage = 1;
+            int totalPages = Integer.MAX_VALUE;
+            List<MovieDto> storeMovies = new ArrayList<>();
+
+            HttpURLConnection connection = null;
+            BufferedReader br = null;
+            do {
+                try {
+                    String apiUrl =
+                        "https://api.themoviedb.org/3/search/movie?query=" + URLEncoder.encode(
+                            keyword,
+                            StandardCharsets.UTF_8) + "&language=ko&page=" + currentPage
+                            + "&api_key="
+                            + apiKey;
+                    URL url = new URL(apiUrl);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+
+                    br = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+
+                    while ((line = br.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    MovieApiResponse movieApiResponse = new Gson().fromJson(response.toString(),
+                        MovieApiResponse.class);
+
+                    if (isResultsEmpty(movieApiResponse.getResults())) {
+                        break;
+                    }
+
+                    storeMovies.addAll(saveMovies(movieApiResponse.getResults()));
+
+                    currentPage++;
+                    totalPages = movieApiResponse.getTotalPages();
+                } catch (IOException e) {
+                    log.error("An error occurred", e);
+                }
+            } while (currentPage <= totalPages);
+            br.close();
+            connection.disconnect();
+            log.warn("저장 완료");
+
+            return storeMovies;
+
+        }
+    }
+
+    public List<MovieDto> getMovieByPerson(String apiKey, String personName) throws IOException {
+        List<MovieDto> storeMovies = new ArrayList<>();
+
+        int currentPage = 1;
+        int totalPages = Integer.MAX_VALUE;
+
+        HttpURLConnection connection = null;
+        BufferedReader br = null;
+        do {
+            try {
+                String apiUrl =
+                    "https://api.themoviedb.org/3/search/person?query=" + URLEncoder.encode(
+                        personName,
+                        StandardCharsets.UTF_8) + "&language=ko&page=" + currentPage
+                        + "&api_key="
+                        + apiKey;
+                URL url = new URL(apiUrl);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
+                StringBuilder response = new StringBuilder();
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    response.append(line);
+                }
+
+                MovieByPersonResponse movieByPersonResponse = new Gson().fromJson(
+                    response.toString(),
+                    MovieByPersonResponse.class);
+
+                List<PersonResponse> personResponses = movieByPersonResponse.getResults();
+
+                for (PersonResponse personResponse : personResponses) {
+                    if (isResultsEmpty(personResponse.getKnownFor())) {
+                        break;
+                    }
+
+                    // db 에 해당 영화가 존재하지 않으면 저장하고 리스트에 추가
+                    // db 에 해당 영화가 존재하면 저장하지 않고 리스트에 추가
+                    for (int i = 0; i < personResponse.getKnownFor().size(); i++) {
+                        if (!movieRepository.existsByMovieId(
+                            personResponse.getKnownFor().get(i).getMovieId())) {
+                            storeMovies.add(MovieDto.fromEntity(movieRepository.save(
+                                personResponse.getKnownFor().get(i).toEntity())));
+                        } else {
+                            storeMovies.add(MovieDto.fromEntity(
+                                personResponse.getKnownFor().get(i).toEntity()));
+                        }
+                    }
+                }
+
+                currentPage++;
+                totalPages = movieByPersonResponse.getTotalPages();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } while (currentPage <= totalPages);
+        br.close();
+        connection.disconnect();
+        log.warn("저장 완료");
+
+        return storeMovies;
+    }
+
+    private boolean isResultsEmpty(List<MovieResultResponse> results) {
+        boolean isEmpty = results == null || results.isEmpty();
+        log.debug("Results are empty: {}", isEmpty);
+        return isEmpty;
+    }
+
+    private List<Movie> toEntity(List<MovieResultResponse> movieResultList) {
         List<Movie> movies = movieResultList.stream()
             .map(MovieResultResponse::toEntity)
             .collect(Collectors.toList());
 
+        return movies;
+    }
+
+    private List<MovieDto> saveMovies(List<MovieResultResponse> movieResultList) {
+        List<Movie> movies = toEntity(movieResultList);
+
         movieRepository.saveAll(movies);
+
+        return movies.stream()
+            .map(MovieDto::fromEntity)
+            .collect(Collectors.toList());
     }
 
 }
