a5a311db1107c21ff46f8da701cd4053f8d8d393
wndgndi <wndgn456@gmail.com>
2024-01-02 21:49:50 +0900

    feat: TMDB API Response

--- src/main/java/com/personal/movie/dto/response/MovieApiResponse.java
+++ src/main/java/com/personal/movie/dto/response/MovieApiResponse.java
@@ -1,5 +1,6 @@
 package com.personal.movie.dto.response;
 
+import com.google.gson.annotations.SerializedName;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,10 +9,16 @@ import lombok.Setter;
 @Setter
 public class MovieApiResponse {
 
+    @SerializedName("page")
     private int page;
+
+    @SerializedName("results")
     private List<MovieResultResponse> results;
 
+    @SerializedName("total_pages")
     private int totalPages;
 
+    @SerializedName("total_results")
     private int totalResults;
+
 }
