117cdf7833ee695eef75439291d3737606019538
wndgndi <wndgn456@gmail.com>
2024-01-10 16:27:43 +0900

    feat: 이미지 entity, repository 구현

---
+++ src/main/java/com/personal/movie/domain/Image.java
@@ -0,0 +1,42 @@
+package com.personal.movie.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Image extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String uploadFileName;
+
+    private String storedFileName;
+
+    private String path;
+
+    private Long fileSize;
+
+    private String extension;
+
+    @ManyToOne
+    @JoinColumn(name = "image_id")
+    private Review review;
+
+    public void setReview(Review review) {
+        this.review = review;
+    }
+}
